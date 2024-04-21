46e3a71eb309760b9abec228c7cea952e610ba7f
Y. Z. Chen <754097987@qq.com>
2024-04-21 23:39:07 +0800

    [Update] buildSrc: add a simple build count tool.
    Took 3 minutes

---
+++ buildSrc/src/main/kotlin/com/geno1024/home/buildsrc/BuildCount.kt
@@ -0,0 +1,16 @@
+package com.geno1024.home.buildsrc
+
+import java.io.File
+
+class BuildCount(val key: String)
+{
+    fun write(i: Int): Int = i.apply {
+        File("$key-count.txt").writeText(toString())
+    }
+
+    fun init() = write(0)
+
+    fun read(): Int = File("$key-count.txt").takeIf(File::exists)?.let { it.readText().trim().toIntOrNull()?:init() }?:init()
+
+    fun inc() = File("$key-count.txt").takeIf(File::exists)?.let { write(it.readText().trim().toIntOrNull()?.inc()?:1) }?:init()
+}
