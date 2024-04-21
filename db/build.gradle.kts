534905acacbf981c86de53ef2a0ec5b62b9274ec
Y. Z. Chen <754097987@qq.com>
2024-04-22 00:06:54 +0800

    [Update] db: build count run successfully.
    Took 28 seconds

--- db/build.gradle.kts
+++ db/build.gradle.kts
@@ -1,3 +1,31 @@
+import com.geno1024.home.buildsrc.BuildCount
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm")
+}
+
+val runCount = BuildCount("run")
+val jarCount = BuildCount("jar")
+
+tasks.register("runCount") {
+    group = "buildcount"
+    doLast {
+        runCount.inc()
+    }
+}
+
+tasks.register("jarCount") {
+    group = "buildcount"
+    doLast {
+        jarCount.inc()
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    dependsOn(tasks.getByName("runCount"))
+}
+
+tasks.withType<Jar> {
+    dependsOn(tasks.getByName("jarCount"))
 }
